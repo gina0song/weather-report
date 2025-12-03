4524992ff725e73cd602961354fe5f3ac125ec64
Gina Song <gina0song@gmail.com>
2025-12-03 11:55:06 -0800

    impletement function to change both text and background of the temperature

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const tempDisplay = document.getElementById('tempValue');
 //update display
 function updateTemperatureDisplay() {
   tempDisplay.textContent = currentTemperature;
+  updateTemperatureColor();
 }
 
 //add click listeners to the spans
@@ -26,3 +27,43 @@ decreaseTempControl.addEventListener('click', () => {
 
 // Initialize
 updateTemperatureDisplay();
+
+
+
+// function to change both text and background of the temperature
+
+// function updateTemperatureColor(){
+//   const tempElement = document.getElementById('tempValue');
+
+//   if (currentTemperature >= 80){
+//     tempElement.style.color = 'red';
+//   } else if (currentTemperature >= 70) {
+//     tempElement.style.color = 'orange'
+//   } else if (currentTemperature >= 60){
+//     tempElement.style.color = 'yellow';
+//   } else if (currentTemperature >= 50){
+//     tempElement.style.color = 'green';
+//   } else {
+//     tempElement.style.color = 'teal';
+//   }
+// }
+
+
+function updateTemperatureColor() {
+  if (currentTemperature >= 80) {
+    tempDisplay.style.backgroundColor = 'red';
+    tempDisplay.style.color = 'white'; 
+  } else if (currentTemperature >= 70) {
+    tempDisplay.style.backgroundColor = 'orange';
+    tempDisplay.style.color = 'white';
+  } else if (currentTemperature >= 60) {
+    tempDisplay.style.backgroundColor = 'yellow';
+    tempDisplay.style.color = 'black';
+  } else if (currentTemperature >= 50) {
+    tempDisplay.style.backgroundColor = 'green';
+    tempDisplay.style.color = 'white';
+  } else {
+    tempDisplay.style.backgroundColor = 'teal';
+    tempDisplay.style.color = 'white';
+  }
+}
