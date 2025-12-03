ea4e22e0c900268b1b7e24ca0f06a3de109d5f33
Helen <lelinewborncare@gmail.com>
2025-12-03 12:04:24 -0800

    add temperature color ranges and dynamic landscapes for Wave 2

--- src/index.js
+++ src/index.js
@@ -7,12 +7,40 @@ let currentTemperature = 72;
 const increaseTempControl = document.getElementById('increaseTempControl');
 const decreaseTempControl = document.getElementById('decreaseTempControl');
 const tempDisplay = document.getElementById('tempValue');
+const landscape = document.getElementById("landscape");
+
 
 //update display
 function updateTemperatureDisplay() {
   tempDisplay.textContent = currentTemperature;
   updateTemperatureColor();
 }
+
+// Temperature color ranges
+if (currentTemperature >= 80) {
+    tempDisplay.style.color = "red";
+  } else if (currentTemperature >= 70) {
+    tempDisplay.style.color = "orange";
+  } else if (currentTemperature >= 60) {
+    tempDisplay.style.color = "goldenrod"; // yellow-ish
+  } else if (currentTemperature >= 50) {
+    tempDisplay.style.color = "green";
+  } else {
+    tempDisplay.style.color = "teal";
+  }
+
+// Landscape ranges
+
+if (currentTemperature >= 80) {
+    landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+  } else if (currentTemperature >= 70) {
+    landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+  } else if (currentTemperature >= 60) {
+    landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+  } else {
+    landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  }
+
 
 //add click listeners to the spans
 increaseTempControl.addEventListener('click' ,() => {
